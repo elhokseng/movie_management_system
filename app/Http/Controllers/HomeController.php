70ed57009434cf2bb40767e1a2d421fda75d5628
seng <hoksengel3@gmail.com>
2024-06-18 19:47:35 +0700

    fixed view homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,31 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Movie;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+use App\Models\Movie;
+use App\Models\Genre;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function Index()
+    public function index()
     {
-        if(Auth::id())
-        {
-            $usertype = Auth()->user()->usertype;
+        if (Auth::id()) {
+            $usertype = Auth::user()->usertype;
 
+            if ($usertype == 'admin') {
+                return view('backend.dashboard.dashboard');
+            } else if ($usertype == 'user') {
+                $datas = Movie::with('genre')->get();
+                $genres = Genre::all();
+                $totalMovies = Movie::count();
 
-            if($usertype == 'admin')
-            {
-                return view('backend.dashboard.dashboard');
-                
-            }
-            else if($usertype == 'user')
-            {
-                // $movies = Movie::all();
-                // return view('dashboard', compact('movies'));
-                return view('backend.dashboard.dashboard');
-            
+                $moviesPerGenre = Movie::select('genre_id', DB::raw('count(*) as total'))
+                    ->groupBy('genre_id')
+                    ->with('genre')
+                    ->get();
+
+                // Attach genre names to the count data
+                $moviesPerGenre = $moviesPerGenre->map(function($item) {
+                    $item->genre_name = Genre::find($item->genre_id)->name;
+                    return $item;
+                });
+
+                return view('backend.dashboard.dashboard', compact('datas', 'totalMovies', 'moviesPerGenre'));
             }
         }
     }
